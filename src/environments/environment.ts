8aae0fd2db5777be0849f0841665536b0c843e43
André Driesel <andre-driesel@gmx.de>
2023-03-21 07:18:53 +0100

    added angular/fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'whatever-firebase',
+    appId: '1:8427603447:web:414f70e53bb6e9680d1af2',
+    databaseURL: 'https://whatever-firebase.firebaseio.com',
+    storageBucket: 'whatever-firebase.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAZRSct1MBwFhBwfW6G_Jz57hf57-dYY_o',
+    authDomain: 'whatever-firebase.firebaseapp.com',
+    messagingSenderId: '8427603447',
+  },
   production: false
 };
 
